0af780b315300177f4edbd0f5ff96dd08750e912
none <none>
2025-08-29 13:07:49 -0700

    Add work on model

--- Model_mtree.v
+++ Model_mtree.v
@@ -1,7 +1,9 @@
 Require Import ZArith.
+Require Import Coq.Program.Equality.
 
 Local Open Scope Z_scope.
 Section model.
+
 
 (*
 A hash is a function that has some nice cryptographic properties
@@ -10,38 +12,160 @@ we only care that the hash function is injective (that every element
 in the domain maps to one element in the codomain.). For purposes of
 modeling, we assume the probability of a hash collision is zero.
 *)
+Parameter data : Type.
+Parameter data_nil: data.
+Parameter hash : Type.
+Parameter hash_func : data -> hash.
+Parameter hash_to_data : hash -> data.
+Axiom hash_func_inj : forall x y: data,
+    hash_func x = hash_func y -> x = y.
+Parameter data_eq_dec : forall x y: data, {x=y} + {x<>y}.
+Parameter hash_eq_dec : forall x y: hash, {x=y} + {x<>y}.
+Parameter data_combine : data -> data -> data.
+Notation "[ x ; .. ; y ]" := (cons x .. (cons y nil) ..).
 
-(* Use numbers for model *)
-Definition hash_func := Z -> Z.
-
-Locate Z.eq.
-Locate eq.
-Print eq.
-Print Z.eq.
-Search Z "eq" "dec".
-Print Z.eq_dec.
-
-Definition hash_func_injective (f: hash_func) := forall (x1 x2: Z),
-((x1 = x2 -> False) -> (f x1 = f x2 -> False)) /\
-((f x1 = f x2) -> (x1 = x2)).
-
-Definition bad_hash_func (input: Z): Z :=
-    0.
-
-Example bad_hash_not_inj: hash_func_injective bad_hash_func -> False.
-Proof.
-    intros.
-    unfold hash_func_injective in H.
-    specialize H with 0 1.
-    unfold bad_hash_func in H.
-    apply H.
-        - intros.
-        discriminate.
-        - reflexivity.
-Qed. 
-    
 (* A merkle tree is a n-ary tree that contains a hash of:
     - the data it points to, if it is a child node.
     - the hashes of all the children, if it is an internal node.
 *)
+Inductive mtree :=
+| Child (h: hash) (d: data)
+| Internal (h: hash) (c: list mtree).
+
+Definition hash_mtree (t: mtree) :=
+match t with
+| Child h _ => h
+| Internal h _ => h
+end.
+
+(*
+Definition test : list Z := [0 ; 2 ; 3].
+*)
+
+Search list "fold".
+Inductive valid_mtree : forall mtree, Prop :=
+| ValidChild : forall (h : hash)(d: data), hash_func d = h -> valid_mtree (Child h d)
+| ValidInternal : forall (h: hash) (xs: list mtree),
+    (xs <> nil) -> (h = hash_func (List.fold_left data_combine (List.map hash_to_data (List.map hash_mtree xs)) data_nil))
+    -> (List.Forall valid_mtree xs)
+    -> valid_mtree (Internal h xs)
+.
+
+(*
+Definition trivial_mtree := Child [0] [0].
+Check trivial_mtree.
+Example trivial_mtree_valid : valid_mtree test_hash (trivial_mtree).
+Proof.
+    apply ValidChild.
+    unfold test_hash.
+    reflexivity.
+Qed.
+
+Definition trivial_mtree_bad := Child test_hash [0] [1 ; 2 ; 3].
+Check trivial_mtree_bad.
+Example trivial_mtree_valid_bad : ~ valid_mtree test_hash (trivial_mtree_bad).
+Proof.
+    unfold not.
+    intros.
+    inversion H.
+    unfold test_hash in H2.
+    discriminate.
+Qed.
+*)
+
+Lemma valid_mtree_children : forall
+    (t: mtree)
+    (h: hash)
+    (xs: list mtree),
+valid_mtree t -> t = Internal h xs -> List.Forall valid_mtree xs.
+Proof.
+    intros.
+    rewrite H0 in H.
+    inversion H.
+    exact H5.
+Qed.
+
+Lemma valid_mtree_subtree : forall
+    (t: mtree)
+    (h: hash)
+    (xs: list mtree)
+    (x: mtree),
+valid_mtree t -> t = Internal h xs -> List.In x xs -> valid_mtree x.
+Proof.
+    intros.
+    assert (List.Forall valid_mtree xs).
+        - apply valid_mtree_children with t h; auto.
+        - rewrite List.Forall_forall in H2.
+        specialize (H2 x).
+        apply H2.
+        apply H1.
+Qed.
+
+Lemma mtree_data_child_inj : forall
+    (t1 t2: mtree)
+    (h1 h2: hash)
+    (d1 d2: data),
+    valid_mtree t1 ->
+    valid_mtree t2 ->
+    t1 = Child h1 d1 ->
+    t2 = Child h2 d2 ->
+    h1 = h2 ->
+    d1 = d2.
+Proof.
+    intros.
+    inversion H; subst.
+    inversion H0; subst.
+    injection H5.
+        - intros.
+        subst.
+        apply hash_func_inj.
+        apply H2.
+        - discriminate. 
+Qed.
+
+Lemma mtree_data_internal_inj : forall
+    (t1 t2: mtree)
+    (h1 h2: hash)
+    (xs1 xs2: list mtree),
+    valid_mtree t1 ->
+    valid_mtree t2 ->
+    t1 = Internal h1 xs1 ->
+    t2 = Internal h2 xs2 ->
+    h1 = h2 ->
+    xs1 = xs2.
+Proof.
+    intros.
+    inversion H; subst.
+        - discriminate.
+        - inversion H0; subst.
+        injection H7.
+        intros.
+        rewrite H1 in H2.
+        apply hash_func_inj in H2.
+        (* In order to prove this, I have to be able to prove
+        that if the combination of hashes is equal, then the
+        hashes that make up the combo are equal.
+        To do that I need to know that;
+            - There is a canonical way to split up a data
+            into a series of hashes.
+            Ex: In reality, hashes are a fixed length of bytes.
+            If I get a combination of hashes, I can split them
+            by partitioning the data by the hash length.
+        *)
+Admitted.
+
+(*
+Check mtree_data_child_inj.
+Lemma mtree_data_inj : forall
+    (t1 t2: mtree),
+    valid_mtree t1 ->
+    valid_mtree t2 ->
+    hash_mtree t1 = hash_mtree t2 ->
+    (exists d1 d2: data, mtree_data_child_inj t1 t2 (hash_mtree t1) (hash_mtree t2) d1 d2)
+    \/ mtree_data_internal_inj t1 t2.
+Proof.
+Admitted.
+*)
+    
+
 End model.
--- mtree.c
+++ mtree.c
@@ -1,3 +1,6 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <openssl/evp.h>
 
 typedef enum mtree_node_type {
     LEAF = 0,
@@ -6,9 +9,72 @@ typedef enum mtree_node_type {
 } mtree_node_type;
 
 typedef struct mtree_node {
+    size_t refcount;
     mtree_node_type type;
+    unsigned char hash[EVP_MAX_MD_SIZE];
+    unsigned int hash_len;
     union {
-        void *data;
+        struct {
+            void *data;
+            size_t len;
+        };
         struct mtree_node *children;
     };
 } mtree_node;
+
+int produce_hash(void *data, size_t len, unsigned char hash[EVP_MAX_MD_SIZE], unsigned int *hash_len);
+
+mtree_node *mtree_child(void *data, size_t len) {
+    mtree_node *node = malloc(sizeof (mtree_node));
+    if (node == NULL) {
+        return NULL;
+    }
+    node->refcount = 1;
+    node->type = LEAF;
+    if (produce_hash(data, len, (node->hash), &(node->hash_len)) != 0) {
+        fprintf(stderr, "Failed to produce hash\n");
+        free(node);
+        return NULL;
+    }
+    node->data = data;
+    node->len = len;
+    return node;
+}
+
+int produce_hash(void *data, size_t len, unsigned char hash[EVP_MAX_MD_SIZE], unsigned int *hash_len) {
+    // Context for digest operations
+    EVP_MD_CTX *ctx = EVP_MD_CTX_new();
+    if (!ctx) {
+        fprintf(stderr, "Failed to create EVP_MD_CTX\n");
+        return 1;
+    }
+
+    // Initialize digest operation for SHA1
+    if (EVP_DigestInit_ex(ctx, EVP_sha1(), NULL) != 1) {
+        fprintf(stderr, "DigestInit failed\n");
+        EVP_MD_CTX_free(ctx);
+        return 1;
+    }
+
+    // Supply the data
+    if (EVP_DigestUpdate(ctx, data, len) != 1) {
+        fprintf(stderr, "DigestUpdate failed\n");
+        EVP_MD_CTX_free(ctx);
+        return 1;
+    }
+
+    // Finalize and get the hash
+    if (EVP_DigestFinal_ex(ctx, hash, hash_len) != 1) {
+        fprintf(stderr, "DigestFinal failed\n");
+        EVP_MD_CTX_free(ctx);
+        return 1;
+    }
+
+    EVP_MD_CTX_free(ctx);
+
+    return 0;
+}
+
+int main (void) {
+    return 0;
+}
